ddef2934935735d9a64936cfe1ab85ea548c573a
r1xez <164931719+r1xez@users.noreply.github.com>
2024-10-31 22:17:31 +0200

    completed

--- String/main.cpp
+++ String/main.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
+string removeAllOccurrences(const string& str, char toRemove) {
+    string result; 
+
+    for (char ch : str) {
+        if (ch != toRemove) { 
+            result += ch;
+        }
+    }
+
+    return result; 
+}
+
 int main() {
-	cout << "Hello, World!" << endl;
-	return 0;
+    string input;
+    cout << "Enter a string: ";
+    getline(cin, input); 
+
+    char toRemove;
+    cout << "Enter the character to remove: ";
+    cin >> toRemove; 
+
+    string result = removeAllOccurrences(input, toRemove); 
+    cout << "Resulting string: " << result << endl; 
+
+    return 0;
 }
