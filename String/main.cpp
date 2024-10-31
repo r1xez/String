19446ac4f1a7b6f5ce877780b1b8623bb814e395
r1xez <164931719+r1xez@users.noreply.github.com>
2024-10-31 22:19:34 +0200

    completed

--- String/main.cpp
+++ String/main.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
+string insertCharacter(const string& str, char toInsert, int position) {
+
+    if (position < 0 || position > str.length()) {
+        cout << "Invalid position!" << endl;
+        return str; 
+    }
+
+    
+    string result = str.substr(0, position) + toInsert + str.substr(position);
+    return result; 
+}
+
 int main() {
-	cout << "Hello, World!" << endl;
-	return 0;
+    string input;
+    cout << "Enter a string: ";
+    getline(cin, input); 
+
+    char toInsert;
+    cout << "Enter the character to insert: ";
+    cin >> toInsert; 
+
+    int position;
+    cout << "Enter the position to insert the character: ";
+    cin >> position; 
+    string result = insertCharacter(input, toInsert, position);
+    cout << "Resulting string: " << result << endl; 
+    return 0;
 }
