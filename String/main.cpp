643753c091f81bc761062b641cf310b628f6fc30
r1xez <164931719+r1xez@users.noreply.github.com>
2024-10-31 22:22:07 +0200

    completed

--- String/main.cpp
+++ String/main.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
+int countCharacterOccurrences(const string& str, char ch) {
+    int count = 0; 
+    for (char c : str) { 
+        if (c == ch) { 
+            count++; 
+        }
+    }
+    return count; 
+}
+
 int main() {
-	cout << "Hello, World!" << endl;
-	return 0;
+    string input;
+    char searchChar;
+
+    cout << "Enter a string: ";
+    getline(cin, input); 
+
+    cout << "Enter a character to search for: ";
+    cin >> searchChar; 
+
+    int occurrences = countCharacterOccurrences(input, searchChar); 
+
+    cout << "The character '" << searchChar << "' seen " << occurrences << " times in the string." << endl; 
+
+    return 0;
 }
