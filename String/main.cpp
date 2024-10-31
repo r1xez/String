2924acd7197b191004408929476e3b3079b382f1
r1xez <164931719+r1xez@users.noreply.github.com>
2024-10-31 22:09:26 +0200

    completed

--- String/main.cpp
+++ String/main.cpp
@@ -1,7 +1,34 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
+void countCharacters(const string& str, int& letters, int& digits, int& others) {
+    letters = 0;
+    digits = 0;
+    others = 0;
+
+    for (char c : str) {
+        if (isalpha(c)) {
+            ++letters;
+        }
+        else if (isdigit(c)) {
+            ++digits;
+        }
+        else {
+            ++others;
+        }
+    }
+}
+
 int main() {
-	cout << "Hello, World!" << endl;
-	return 0;
+    string input;
+    cout << "Enter a string: ";
+    getline(cin, input);
+
+    int letters, digits, others;
+    countCharacters(input, letters, digits, others);
+
+    cout << "Letters: " << letters << ", Digits: " << digits << ", Others: " << others << endl;
+
+    return 0;
 }
