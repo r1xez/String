beb60bff0c77e8383779b60152982c10721489b4
r1xez <164931719+r1xez@users.noreply.github.com>
2024-10-31 22:16:03 +0200

    completed

--- String/main.cpp
+++ String/main.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
+string removeCharacterAtIndex(const string& str, int index) {
+    
+    if (index < 0 || index >= str.length()) {
+        return str; 
+    }
+   
+    return str.substr(0, index) + str.substr(index + 1);
+}
+
 int main() {
-	cout << "Hello, World!" << endl;
-	return 0;
+    string input;
+    cout << "Enter a string: ";
+    getline(cin, input);
+
+    int index;
+    cout << "Enter index of character to remove: ";
+    cin >> index;
+
+    string result = removeCharacterAtIndex(input, index);
+    cout << "Resulting string: " << result << endl;
+
+    return 0;
 }
