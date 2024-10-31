c7bc4b4b2d05d650149621c8a04d949be92c0d20
r1xez <164931719+r1xez@users.noreply.github.com>
2024-10-31 22:11:01 +0200

    completed

--- String/main.cpp
+++ String/main.cpp
@@ -1,7 +1,34 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
+int countWords(const string& str) {
+    int count = 0;
+    bool inWord = false; 
+
+    for (char c : str) {
+       
+        if (c != ' ' && c != '\t') {
+            if (!inWord) {
+                inWord = true; 
+                ++count; 
+            }
+        }
+        else {
+            inWord = false;
+        }
+    }
+
+    return count;
+}
+
 int main() {
-	cout << "Hello, World!" << endl;
-	return 0;
+    string input;
+    cout << "Enter a sentence: ";
+    getline(cin, input);
+
+    int wordCount = countWords(input);
+    cout << "Number of words: " << wordCount << endl;
+
+    return 0;
 }
