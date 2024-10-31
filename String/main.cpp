302e4ec0438c83969fd8254ee2e3226b2ba1dd3d
r1xez <164931719+r1xez@users.noreply.github.com>
2024-10-31 22:08:22 +0200

    completed

--- String/main.cpp
+++ String/main.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
+void replaceSpacesWithTabs(string& str) {
+    for (char& c : str) {
+        if (c == ' ') {
+            c = '\t'; 
+            
+        }
+    }
+}
+
 int main() {
-	cout << "Hello, World!" << endl;
-	return 0;
+    string input;
+    cout << "Enter a string: ";
+    getline(cin, input);
+
+    replaceSpacesWithTabs(input);
+    cout << "Modified string: " << input << endl;
+
+    return 0;
 }
