e941be276d77327a82c0558b7ad2bd0cf5ce71a4
r1xez <164931719+r1xez@users.noreply.github.com>
2024-10-31 22:23:28 +0200

    completed

--- String/main.cpp
+++ String/main.cpp
@@ -1,7 +1,39 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
+void countCharacters(const string& str, int& letterCount, int& digitCount, int& otherCount) {
+    letterCount = 0; 
+    digitCount = 0; 
+    otherCount = 0; 
+
+    for (char c : str) {
+        // Перевірка, чи символ є літерою
+        if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) {
+            letterCount++;
+        }
+       
+        else if (c >= '0' && c <= '9') {
+            digitCount++;
+        }
+        
+        else {
+            otherCount++;
+        }
+    }
+}
+
 int main() {
-	cout << "Hello, World!" << endl;
-	return 0;
+    string input;
+    cout << "Enter a string: ";
+    getline(cin, input); 
+
+    int letterCount, digitCount, otherCount;
+    countCharacters(input, letterCount, digitCount, otherCount);
+
+    cout << "Letters: " << letterCount << endl; 
+    cout << "Digits: " << digitCount << endl; 
+    cout << "Other characters: " << otherCount << endl;
+
+    return 0;
 }
